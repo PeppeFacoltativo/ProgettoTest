c8789f7373ff7e8f3d510cfbe4c032b2cf62d732
PeppeFacoltativo <simogabri1999@gmail.com>
2017-07-04 15:41:28 +0200

    1) Movimento di player e camera tramite Mouse; 2) Dash caricato (max 3 secondi).

--- ProvaController/Assets/scripts/camera.cs
+++ ProvaController/Assets/scripts/camera.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class camera : MonoBehaviour {
 
-    public GameObject player;       //Public variable to store a reference to the player game object
+    public Transform player;       //Public variable to store a reference to the player game object
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
 
     // Use this for initialization
     void Start()
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+        offset = new Vector3(player.position.x, player.position.y + 2.0f, player.position.z + 9.0f);
     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        float movement = Input.GetAxis("Horizontal") * Time.deltaTime;
+        if (!Mathf.Approximately(movement, 0f))
+        {
+            transform.RotateAround(player.position, Vector3.up, movement);
+            offset = transform.position - player.position;
+        }
     }
 }
--- ProvaController/Assets/scripts/controller.cs
+++ ProvaController/Assets/scripts/controller.cs
@@ -4,46 +4,101 @@ using System.Collections;
 public class controller : MonoBehaviour {
 
 	// Use this for initialization
+
 	void Start () {
-        
+        Cursor.visible = false;
     }
 
+    [SerializeField]
     float speed = 0.1F;
-    float dash = 0.2F;
-    int dashmultiplier = 1;
+
+    [SerializeField]
     float jumpForce = 15f;
 
+    [SerializeField]
+    float trampForce = 80f;
+
+    [SerializeField]
+    float dash = 0.5F;
+
+    float dashmultiplier = 1.5f;
+    bool grounded = true;
+    bool dashing = false;
+    float dashTime = 0;
+    float startTime = 0;
 
     // Update is called once per frame
-    void Update () {
-
+    void FixedUpdate () {
+        
+        //Player
         Rigidbody rb = GetComponent<Rigidbody>();
 
-        float Vmov=0;
-        float Hmov = 0;
-        float jump = 0;
-        
+        //Get Horizontal Rotation from mouse
+        float Hrot = 0;
+        Hrot = Input.GetAxisRaw("Mouse X");
 
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            dash = 0.2F;
+            startTime = Time.time;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            dashTime = Time.time - startTime;
+            //Maximum dashTime = 3 sec
+            if (dashTime > 3)
+            {
+                dashTime = 3;
+            }
+
+            dashing = true;
+        }
+
+        if (dashing)
+        {
+            dashMove();
         }
         else
         {
-            dash = 0;
+            Move();
         }
 
-        Vmov = Input.GetAxis("Vertical") * (speed+dash) * dashmultiplier;
-        Hmov = Input.GetAxis("Horizontal") * (speed+dash) * dashmultiplier;
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
         }
+        transform.Rotate(new Vector3(0, Hrot, 0));
 
-        transform.Translate(Hmov, jump, Vmov); 
+    }
 
-	}
+    public void Move()
+    {
+        float Vmov = 0;
+        float Hmov = 0;
+        Vmov = Input.GetAxis("Vertical") * speed * dashmultiplier;
+        Hmov = Input.GetAxis("Horizontal") * speed;
+        transform.Translate(Hmov, 0, Vmov);
+    }
+
+    public void dashMove()
+    {
+        if (dashTime>0)
+        {
+            float Vmov = 0;
+            float Hmov = 0;
+            Vmov = (speed + dash) * dashmultiplier;
+            Hmov = Input.GetAxis("Horizontal") * speed;
+            transform.Translate(Hmov, 0, Vmov);
+            dashTime = dashTime-Time.deltaTime;
+        }
+        else
+        {
+            startTime = 0;
+            dashTime = 0;
+            dashing = false;
+        }   
+    }
+
 
     void OnTriggerEnter(Collider box)
     {
@@ -55,7 +110,12 @@ public class controller : MonoBehaviour {
 
         if (box.tag=="tramp")
         {
-            jumpForce = 80;
+            jumpForce = trampForce;
+        }
+
+        if (box.tag=="ground")
+        {
+            grounded = true;
         }
     }
 
@@ -70,6 +130,11 @@ public class controller : MonoBehaviour {
         if (box.tag == "tramp")
         {
             jumpForce = 15;
+        }
+
+        if (box.tag == "ground")
+        {
+            grounded = false;
         }
     }
 
